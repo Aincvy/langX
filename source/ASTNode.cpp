c159653a4ceaf5000df65be9908ca5aa2c5c55da
The World <474551869@qq.com>
2016-07-04 21:53:21 +0800

    第一次提交， 随便写了一些内容

---
+++ source/ASTNode.cpp
@@ -0,0 +1,26 @@
+#include "..\include\ASTNode.h"
+
+namespace langX {
+	ASTNode::ASTNode(const std::string & value)
+	{
+		this->m_value = std::string(value);
+	}
+	ASTNode::ASTNode(const char * value)
+	{
+		this->m_value = std::string(value);
+	}
+	ASTNode::ASTNode(const char * value, ASTNode * parent)
+	{
+		this->m_value = std::string(value);
+		this->m_parent = parent;
+	}
+	std::string ASTNode::value() const
+	{
+		return (this->m_value);
+	}
+	ASTNode * ASTNode::parent() const
+	{
+		return this->m_parent;
+	}
+}
+
