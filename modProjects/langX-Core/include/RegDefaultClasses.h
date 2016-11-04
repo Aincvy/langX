c56911f434b6a95ef1ca743134a34d494869f918
Aincvy <aincvy@gmail.com>
2016-11-04 15:17:52 +0800

    实现了 Stack, Math, Queue 类， 但是并没有进行测试。

---
+++ modProjects/langX-Core/include/RegDefaultClasses.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../../../include/langX.h"
+#include "../../../include/XNameSpace.h"
+
+namespace langX {
+
+	// 注册默认的类
+
+	// 注册 Math 类
+	int regMath(langXState *state, XNameSpace* space);
+
+}
