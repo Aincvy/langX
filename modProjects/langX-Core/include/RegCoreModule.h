496094eabbee030d94947522e2c9269bf812d05a
The World <474551869@qq.com>
2016-11-04 20:05:46 +0800

    提交vector

--- modProjects/langX-Core/include/RegCoreModule.h
+++ modProjects/langX-Core/include/RegCoreModule.h
@@ -23,6 +23,9 @@ namespace langX {
 	// 
 	int regStack(langXState *state, XNameSpace* space);
 
+	//
+	int regVector(langXState *state, XNameSpace* space);
+
 	//  core 库的东西 ，上面都是core.util 里面的东西
 	int regDeafult(langXState *state, XNameSpace* space);
 }
