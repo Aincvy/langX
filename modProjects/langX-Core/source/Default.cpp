e968af67accfc6a79e1257daaf70a36c47e81574
hideDragon <474551869@qq.com>
2016-11-04 15:17:52 +0800

    实现了 Stack, Math, Queue 类， 但是并没有进行测试。

--- modProjects/langX-Core/source/Default.cpp
+++ modProjects/langX-Core/source/Default.cpp
@@ -1,9 +1,12 @@
 #include "../include/RegCoreModule.h"
+#include "../include/RegDefaultClasses.h"
 
 namespace langX {
 
 
 	int regDeafult(langXState *state, XNameSpace* space) {
+
+		regMath(state, space);
 
 		return 0;
 	}
