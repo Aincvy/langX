b34dfadeccad63861d7ffac374e66431a036e157
Aincvy <aincvy@gmail.com>
2016-11-06 19:25:17 +0800

    初始化 Mysql库

---
+++ modProjects/langX-Mysql/include/MysqlModule.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "../../../include/X3rdModule.h"
+#include "../../../include/langX.h"
+
+namespace langX {
+
+	class MysqlModule: public X3rdModule
+	{
+	public:
+		MysqlModule();
+		~MysqlModule();
+
+		// 初始化 core模块
+		int init(langXState *);
+
+		// 卸载c ore模块
+		int unload(langXState *);
+
+	private:
+
+	};
+
+
+}
---
+++ modProjects/langX-Mysql/source/DataTable.cpp
@@ -0,0 +1,11 @@
+#include "../include/RegMysqlModule.h"
+
+namespace langX {
+
+	int regDataTable(langXState *state, XNameSpace* space) {
+
+
+		return 0;
+	}
+
+}
