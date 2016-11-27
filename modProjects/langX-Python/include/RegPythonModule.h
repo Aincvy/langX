1383efecc08dc23c0bbc753a76f9686253ee3408
Aincvy <aincvy@gmail.com>
2016-11-27 19:04:15 +0800

    简单的弄了一下 python 库。

--- modProjects/langX-Python/include/RegPythonModule.h
+++ modProjects/langX-Python/include/RegPythonModule.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include "../../../include/langX.h"
+#include "../../../include/XNameSpace.h"
+
+
 namespace langX {
 
 
+
+	int regPythonLib(langXState *state, XNameSpace* space);
+
+
+	int regPythonHook(langXState *state, XNameSpace* space);
 
 }
