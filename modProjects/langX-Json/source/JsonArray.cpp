5d2ab155a649d4a757ca7ee26d2255e5eaf1edf7
unknown <>
2017-01-30 14:50:38 +0800

    对json库的一些代码进行一些格式化操作

--- modProjects/langX-Json/source/JsonArray.cpp
+++ modProjects/langX-Json/source/JsonArray.cpp
@@ -10,168 +10,157 @@
 namespace langX {
 
 
-Object * langX_JsonArray_setItem(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_setItem error! NO OBJ!\n");
+	Object * langX_JsonArray_setItem(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_setItem error! NO OBJ!\n");
+			return nullptr;
+		}
+
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_put(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_put error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_put(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_put error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_getString(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_getString error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_getString(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_getString error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_getNumber(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_getNumber error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_getNumber(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_getNumber error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_toStringArray(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_toStringArray error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_toStringArray(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_toStringArray error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_toNumberArray(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_toNumberArray error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_toNumberArray(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_toNumberArray error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_size(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_size error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_size(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_size error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_JsonArray_Dtor(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_JsonArray_Dtor error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_JsonArray_Dtor(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_JsonArray_Dtor error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_getJsonArray(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_getJsonArray error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_getJsonArray(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_getJsonArray error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_getJsonObject(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_getJsonObject error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_getJsonObject(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_getJsonObject error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonArray_JsonArray(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonArray_JsonArray error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonArray_JsonArray(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonArray_JsonArray error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
-  
-  int regJsonArray(langXState *state, XNameSpace* space) {
-	
-	ClassInfo *info = new ClassInfo("JsonArray");	
-info->addFunction("setItem", create3rdFunc("setItem", langX_JsonArray_setItem));
+	int regJsonArray(langXState *state, XNameSpace* space) {
 
-info->addFunction("put", create3rdFunc("put", langX_JsonArray_put));
+		ClassInfo *info = new ClassInfo("JsonArray");
+		info->addFunction("setItem", create3rdFunc("setItem", langX_JsonArray_setItem));
+		info->addFunction("put", create3rdFunc("put", langX_JsonArray_put));
+		info->addFunction("getString", create3rdFunc("getString", langX_JsonArray_getString));
+		info->addFunction("getNumber", create3rdFunc("getNumber", langX_JsonArray_getNumber));
+		info->addFunction("toStringArray", create3rdFunc("toStringArray", langX_JsonArray_toStringArray));
+		info->addFunction("toNumberArray", create3rdFunc("toNumberArray", langX_JsonArray_toNumberArray));
+		info->addFunction("size", create3rdFunc("size", langX_JsonArray_size));
+		info->addFunction("~JsonArray", create3rdFunc("~JsonArray", langX_JsonArray_JsonArray_Dtor));
+		info->addFunction("getJsonArray", create3rdFunc("getJsonArray", langX_JsonArray_getJsonArray));
+		info->addFunction("getJsonObject", create3rdFunc("getJsonObject", langX_JsonArray_getJsonObject));
+		info->addFunction("JsonArray", create3rdFunc("JsonArray", langX_JsonArray_JsonArray));
 
-info->addFunction("getString", create3rdFunc("getString", langX_JsonArray_getString));
+		space->putClass(info);
 
-info->addFunction("getNumber", create3rdFunc("getNumber", langX_JsonArray_getNumber));
-
-info->addFunction("toStringArray", create3rdFunc("toStringArray", langX_JsonArray_toStringArray));
-
-info->addFunction("toNumberArray", create3rdFunc("toNumberArray", langX_JsonArray_toNumberArray));
-
-info->addFunction("size", create3rdFunc("size", langX_JsonArray_size));
-
-info->addFunction("~JsonArray", create3rdFunc("~JsonArray", langX_JsonArray_JsonArray_Dtor));
-
-info->addFunction("getJsonArray", create3rdFunc("getJsonArray", langX_JsonArray_getJsonArray));
-
-info->addFunction("getJsonObject", create3rdFunc("getJsonObject", langX_JsonArray_getJsonObject));
-
-info->addFunction("JsonArray", create3rdFunc("JsonArray", langX_JsonArray_JsonArray));
-
-	
-	space->putClass(info);
-	
-  	return 0;
-  }
+		return 0;
+	}
 }
 
 
--- modProjects/langX-Json/source/JsonObject.cpp
+++ modProjects/langX-Json/source/JsonObject.cpp
@@ -10,112 +10,105 @@
 namespace langX {
 
 
-Object * langX_JsonObject_getJsonArray(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonObject_getJsonArray error! NO OBJ!\n");
+	Object * langX_JsonObject_getJsonArray(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonObject_getJsonArray error! NO OBJ!\n");
+			return nullptr;
+		}
+
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonObject_put(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonObject_put error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonObject_put(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonObject_put error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonObject_getString(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonObject_getString error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonObject_getString(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonObject_getString error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonObject_getNumber(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonObject_getNumber error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonObject_getNumber(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonObject_getNumber error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonObject_getJsonObject(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonObject_getJsonObject error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonObject_getJsonObject(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonObject_getJsonObject error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonObject_JsonObject_Dtor(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonObject_JsonObject_Dtor error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonObject_JsonObject_Dtor(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonObject_JsonObject_Dtor error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
+	Object * langX_JsonObject_JsonObject(X3rdFunction *func, const X3rdArgs &args) {
+		if (args.object == nullptr)
+		{
+			printf("langX_JsonObject_JsonObject error! NO OBJ!\n");
+			return nullptr;
+		}
 
-Object * langX_JsonObject_JsonObject(X3rdFunction *func, const X3rdArgs &args) {
-	if (args.object == nullptr)
-	{
-		printf("langX_JsonObject_JsonObject error! NO OBJ!\n");
 		return nullptr;
 	}
 
-	return nullptr;
-}
 
 
-  
-  int regJsonObject(langXState *state, XNameSpace* space) {
-	
-	ClassInfo *info = new ClassInfo("JsonObject");	
-info->addFunction("getJsonArray", create3rdFunc("getJsonArray", langX_JsonObject_getJsonArray));
+	int regJsonObject(langXState *state, XNameSpace* space) {
 
-info->addFunction("put", create3rdFunc("put", langX_JsonObject_put));
+		ClassInfo *info = new ClassInfo("JsonObject");
+		info->addFunction("getJsonArray", create3rdFunc("getJsonArray", langX_JsonObject_getJsonArray));
+		info->addFunction("put", create3rdFunc("put", langX_JsonObject_put));
+		info->addFunction("getString", create3rdFunc("getString", langX_JsonObject_getString));
+		info->addFunction("getNumber", create3rdFunc("getNumber", langX_JsonObject_getNumber));
+		info->addFunction("getJsonObject", create3rdFunc("getJsonObject", langX_JsonObject_getJsonObject));
+		info->addFunction("~JsonObject", create3rdFunc("~JsonObject", langX_JsonObject_JsonObject_Dtor));
+		info->addFunction("JsonObject", create3rdFunc("JsonObject", langX_JsonObject_JsonObject));
 
-info->addFunction("getString", create3rdFunc("getString", langX_JsonObject_getString));
+		space->putClass(info);
 
-info->addFunction("getNumber", create3rdFunc("getNumber", langX_JsonObject_getNumber));
-
-info->addFunction("getJsonObject", create3rdFunc("getJsonObject", langX_JsonObject_getJsonObject));
-
-info->addFunction("~JsonObject", create3rdFunc("~JsonObject", langX_JsonObject_JsonObject_Dtor));
-
-info->addFunction("JsonObject", create3rdFunc("JsonObject", langX_JsonObject_JsonObject));
-
-	
-	space->putClass(info);
-	
-  	return 0;
-  }
+		return 0;
+	}
 }
 
 
