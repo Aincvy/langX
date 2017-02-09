abe854df97d091f8249834bf381ded2d07a467bb
Aincvy <aincvy@gmail.com>
2017-02-09 22:05:44 +0800

    瞎写了一点内容

--- modProjects/langX-Mysql/include/RegMysqlModule.h
+++ modProjects/langX-Mysql/include/RegMysqlModule.h
@@ -1,14 +1,29 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 #include "../../../include/langX.h"
 #include "../../../include/XNameSpace.h"
 
 namespace langX {
+
+	class langXObject;
 
 	int regMysqlClient(langXState *state , XNameSpace* space);
 
 	int regDataTable(langXState *state, XNameSpace* space);
 
 	int regDataRow(langXState *state, XNameSpace* space);
+
+
+	///  数据表结构
+	struct DataTable
+	{
+		// row data . every item is a DataRow object.
+		std::vector<langXObject*> rows;
+		// column names. 
+		std::vector<std::string> columns;
+	};
 
 }
