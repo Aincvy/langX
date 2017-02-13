2c13d5c36b8b797f4e1abcecfe15e0d5327cb8e3
Aincvy <aincvy@gmail.com>
2017-02-13 19:55:06 +0800

    尝试实现了一下 时间和随机数的基本支持

--- modProjects/langX-Core/source/Random.cpp
+++ modProjects/langX-Core/source/Random.cpp
@@ -7,9 +7,26 @@
 #include "../../../include/langXObjectRef.h"
 #include "../../../include/Allocator.h"
 #include "../../../include/Number.h"
+#include "../../../include/Utils.h"
+
+static const long addend = 0xBL;
+static const long mask = (1L << 48) - 1;
+static const long multiplier = 0x5FECCE00DL;
+static const double DOUBLE_UNIT = 0x1.0p-53;
 
 namespace langX {
 
+
+	int next(int bits, langXObject *object ) {
+
+		long *i = (long*)object->get3rdObj();
+
+		long oldseed = *i;
+		long nextseed = (oldseed * multiplier + addend) & mask;
+		*i = nextseed;
+
+		return (int)(nextseed >> (48 - bits));
+	}
 
 	Object * langX_Random_nextDouble(X3rdFunction *func, const X3rdArgs &args) {
 		if (args.object == nullptr)
@@ -18,7 +35,11 @@ namespace langX {
 			return nullptr;
 		}
 
-		return nullptr;
+		langXObject *obj = args.object;
+
+		double a = (((long)next(26, obj)) << 27 + next(27, obj)) * DOUBLE_UNIT;
+
+		return getState()->getAllocator().allocateNumber(a);
 	}
 
 
@@ -30,7 +51,19 @@ namespace langX {
 			return nullptr;
 		}
 
-		return nullptr;
+		
+		int abc = next(32, args.object);
+
+		Object *a = args.args[0];
+		if (a && a->getType() == NUMBER)
+		{
+			int max = ((Number*)a)->getIntValue();
+
+			for (; abc > max; abc %= max);   // 分号在这
+			
+		}
+
+		return getState()->getAllocator().allocateNumber(abc);
 	}
 
 
@@ -42,6 +75,10 @@ namespace langX {
 			return nullptr;
 		}
 
+		long *i = (long*)args.object->get3rdObj();
+		free(i);
+		args.object->set3rdObj(nullptr);
+
 		return nullptr;
 	}
 
@@ -52,6 +89,19 @@ namespace langX {
 		{
 			printf("langX_Random_Random error! NO OBJ!\n");
 			return nullptr;
+		}
+
+		long *i = (long*)calloc(1, sizeof(long));
+		args.object->set3rdObj(i);
+
+		Object *a = args.args[0];
+		if ( a && a->getType() == NUMBER)
+		{
+			int val = ((Number*)a)->getIntValue();
+			*i = val;
+		}
+		else {
+			*i = getTime();
 		}
 
 		return nullptr;
