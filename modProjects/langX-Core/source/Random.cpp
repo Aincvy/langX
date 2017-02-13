b7c8ec6e7ffae92e4a87c74ef88e816e6d499595
Aincvy <aincvy@gmail.com>
2017-02-13 21:03:25 +0800

    修正了随机数的实现

--- modProjects/langX-Core/source/Random.cpp
+++ modProjects/langX-Core/source/Random.cpp
@@ -1,4 +1,6 @@
-﻿#include "../include/RegCoreModule.h"
+﻿#include <random>
+
+#include "../include/RegCoreModule.h"
 
 #include "../../../include/ClassInfo.h"
 #include "../../../include/YLlangX.h"
@@ -9,24 +11,8 @@
 #include "../../../include/Number.h"
 #include "../../../include/Utils.h"
 
-static const long addend = 0xBL;
-static const long mask = (1L << 48) - 1;
-static const long multiplier = 0x5FECCE00DL;
-static const double DOUBLE_UNIT = 0x1.0p-53;
-
 namespace langX {
 
-
-	int next(int bits, langXObject *object ) {
-
-		long *i = (long*)object->get3rdObj();
-
-		long oldseed = *i;
-		long nextseed = (oldseed * multiplier + addend) & mask;
-		*i = nextseed;
-
-		return (int)(nextseed >> (48 - bits));
-	}
 
 	Object * langX_Random_nextDouble(X3rdFunction *func, const X3rdArgs &args) {
 		if (args.object == nullptr)
@@ -35,11 +21,12 @@ namespace langX {
 			return nullptr;
 		}
 
-		langXObject *obj = args.object;
+		std::mt19937 *gen = (std::mt19937 *)args.object->get3rdObj();
+		std::random_device rd;
+		
+		double abc = std::uniform_real_distribution<double>(0.0,1.0)((*gen));
 
-		double a = (((long)next(26, obj)) << 27 + next(27, obj)) * DOUBLE_UNIT;
-
-		return getState()->getAllocator().allocateNumber(a);
+		return getState()->getAllocator().allocateNumber(abc);
 	}
 
 
@@ -51,16 +38,19 @@ namespace langX {
 			return nullptr;
 		}
 
+		std::mt19937 *gen = (std::mt19937 *)args.object->get3rdObj();
+		std::random_device rd;
 		
-		int abc = next(32, args.object);
-
+		int abc = 0;
 		Object *a = args.args[0];
 		if (a && a->getType() == NUMBER)
 		{
 			int max = ((Number*)a)->getIntValue();
 
-			for (; abc > max; abc %= max);   // 分号在这
-			
+			abc = std::uniform_int_distribution<int>(0,max)((*gen));
+		}
+		else {
+			abc = std::uniform_int_distribution<int>(0 )((*gen));
 		}
 
 		return getState()->getAllocator().allocateNumber(abc);
@@ -75,8 +65,8 @@ namespace langX {
 			return nullptr;
 		}
 
-		long *i = (long*)args.object->get3rdObj();
-		free(i);
+		std::mt19937 *gen = (std::mt19937 *)args.object->get3rdObj();
+		delete gen;
 		args.object->set3rdObj(nullptr);
 
 		return nullptr;
@@ -91,17 +81,17 @@ namespace langX {
 			return nullptr;
 		}
 
-		long *i = (long*)calloc(1, sizeof(long));
-		args.object->set3rdObj(i);
+		std::mt19937 *gen = new std::mt19937();
+		args.object->set3rdObj(gen);
 
 		Object *a = args.args[0];
 		if ( a && a->getType() == NUMBER)
 		{
 			int val = ((Number*)a)->getIntValue();
-			*i = val;
+			gen->seed(val);
 		}
 		else {
-			*i = getTime();
+			gen->seed(getTime());
 		}
 
 		return nullptr;
