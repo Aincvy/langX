70914448935d0a620bee401f0dbee1f7c3e3839d
The World <world@aincvy.com>
2018-02-28 19:07:46 +0800

    zlib init

---
+++ modProjects/langX-Zlib/include/ZlibModule.h
@@ -0,0 +1,22 @@
+/*************************************************************************************//**
+ *  @file       ZlibModule.h
+ *
+ *  @brief      Brief description of ZlibModule.h
+ *
+ *  @date       2018-02-28 19:05
+ *         
+ **************************************************************************************/
+
+
+#ifndef ZLIBMODULE_H
+#define ZLIBMODULE_H
+
+namespace langX{
+
+
+
+}
+
+
+#endif /* !ZLIBMODULE_H */
+
